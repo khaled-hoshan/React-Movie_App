7304882013e18cd1b426833a36f916151a242324
Khaled_Hoshan <khaledjmk617@gmail.com>
2024-05-19 02:26:12 +0300

    add dynamic code to map over movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 const API_URL = "http://www.omdbapi.com?apikey=7d0bc5e0";
@@ -13,11 +13,13 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const fetchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
   useEffect(() => {
     fetchMovies("Batman");
@@ -29,9 +31,17 @@ const App = () => {
         <input placeholder="Search..." value="superman" onChange={() => {}} />
         <img src="" alt="search" onClick={() => {}} />
 
-        <div className="container">
-          <MovieCard movie1={movie1} />
-        </div>
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie1={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>NOT FOUND</h2>
+          </div>
+        )}
       </div>
     </div>
   );
